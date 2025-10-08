b8bb6e243d9a552c00e4f8ce1ff5a109a6684351
Kush Gupta <kush.g@grexit.com>
2025-10-08 16:24:15 +0530

    uploaded main.go server code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kushgupta-hiver/TTT/internal/engine"
+	"github.com/kushgupta-hiver/TTT/internal/transport/ws"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr := ":8000"
+	if v := os.Getenv("ADDR"); v != "" {
+		addr = v
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("TicTacToe WS server. Connect via /ws\n"))
+	}))
+	mux.Handle("/ws", ws.NewServer(ws.Config{}, engine.NewEngine()))
+
+	log.Printf("listening on %s ...", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
 }
